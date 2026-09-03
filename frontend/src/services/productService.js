f9e82b28a59f11006e55147e7883ac95107cbb05
akshay <jorrigala.akshay@gmail.com>
2026-09-03 21:39:14 +0530

    Fix frontend backend API connections

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5002/api/products";
+const API_URL = "http://localhost:5000/api/products";
 
 export const getProducts = async () => {
   const response = await axios.get(API_URL);
