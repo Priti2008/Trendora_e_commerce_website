cb85513c28b65d367b899ad27621cda2f7d19b98
231fa18296-dev <231fa18296@gmail.com>
2026-09-09 12:53:40 +0530

    Clean up server.js by removing extra newline
    Removed an unnecessary newline to clean up the code.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
 const app = express();
 
 const PORT = 5000;
