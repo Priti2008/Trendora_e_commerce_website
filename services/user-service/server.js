ae369ffe98e663a9161d5ccb82a56710841841e4
231fa18296-dev <231fa18296@gmail.com>
2026-09-09 12:55:22 +0530

    Remove unnecessary  lines from server.js

--- services/user-service/server.js
+++ services/user-service/server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
